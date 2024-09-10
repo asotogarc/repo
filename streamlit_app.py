dcc2077e73cbafad3389f37d0bd3a7f3c9bca8a0
asotogarc <103063017+asotogarc@users.noreply.github.com>
2024-09-10 13:45:21 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,13 @@
 import streamlit as st
-from ultralytics import YOLO
 from PIL import Image
 import numpy as np
+import torch
 
-@st.cache_resource
-def load_model():
-    return YOLO('yolov8n.pt')  # Carga el modelo YOLOv8 nano
+# Importing YOLO directly from torch hub to avoid OpenCV dependency
+model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
 
 def main():
-    st.title("Detección de Objetos con YOLOv8")
-    
-    model = load_model()
+    st.title("Detección de Objetos con YOLOv5")
     
     # Usar st.camera_input para capturar una imagen de la webcam
     img_file_buffer = st.camera_input("Toma una foto")
@@ -19,23 +16,18 @@ def main():
         # Convertir la imagen a un objeto PIL Image
         image = Image.open(img_file_buffer)
         
-        # Convertir la imagen PIL a un array de numpy
-        img_array = np.array(image)
-        
         # Realizar la detección de objetos
-        results = model(img_array)
+        results = model(image)
         
-        # Visualizar los resultados
-        for r in results:
-            im_array = r.plot()  # Plot los resultados con las cajas delimitadoras
-            im = Image.fromarray(im_array[..., ::-1])  # Convertir de BGR a RGB
+        # Obtener la imagen con las detecciones
+        img_with_boxes = Image.fromarray(results.render()[0])
         
         # Mostrar la imagen con las detecciones
-        st.image(im, caption="Imagen con Detecciones", use_column_width=True)
+        st.image(img_with_boxes, caption="Imagen con Detecciones", use_column_width=True)
         
         # Mostrar las clases detectadas
-        detected_classes = [model.names[int(c)] for c in results[0].boxes.cls]
-        st.write("Objetos detectados:", ', '.join(set(detected_classes)))
+        detected_classes = results.pandas().xyxy[0]['name'].unique()
+        st.write("Objetos detectados:", ', '.join(detected_classes))
     
     else:
         st.info("Esperando a que tomes una foto...")
