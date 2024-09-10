beedf0de10a59d4477a037a802ba9d64a81f3382
asotogarc <103063017+asotogarc@users.noreply.github.com>
2024-09-10 13:23:05 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
-  model = YOLO("yolov8n.pt")
-  res = model.predict(file,conf=0.5,save=True)
-  box = res[0].boxes.xyxy.tolist()
-  res_plotted = res[0].plot()[:, :, ::-1]
-  st.image(res_plotted, caption='Text Detections',use_column_width=True)
-  st.write("Number of the Detections : "+str(len(box)))
+model = YOLO("yolov8n.pt")
+res = model.predict(file,conf=0.5,save=True)
+box = res[0].boxes.xyxy.tolist()
+res_plotted = res[0].plot()[:, :, ::-1]
+st.image(res_plotted, caption='Text Detections',use_column_width=True)
+st.write("Number of the Detections : "+str(len(box)))
